c66ddddbd80d0d628c561d652482e510e218b04c
rnld-devcode <rizkyrinaldy.schwork@gmail.com>
2026-08-17 13:43:28 +0800

    fix: clean up next.config.ts options

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // Mengabaikan error TypeScript saat build di Vercel
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Mengabaikan error ESLint juga biar aman
-    ignoreDuringBuilds: true,
   },
 };
 
