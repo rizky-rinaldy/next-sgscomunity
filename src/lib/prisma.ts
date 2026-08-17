5e18e84171561ba6a06b5e7b09fe183f8e5cc35e
rnld-devcode <rizkyrinaldy.schwork@gmail.com>
2026-08-17 13:37:09 +0800

    fix: safe prisma client initialization for build

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,9 +5,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["error"], // Biar kalau ada error database, keliatan jelas di terminal
+    log: ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
